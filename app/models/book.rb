c358fb8639d52b65184131ad13dd05c5d2445484
Sibevin Wang <sibevin@gmail.com>
2014-04-09 08:51:53 +0800

    Support partial date in edit page
    DESC: F

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,13 +1,30 @@
 class Book < ActiveRecord::Base
+  PartialDate = Struct.new(:year, :month, :day)
+
   def release_date_display
     if self.date_mask == 4
       return self.release_date.strftime('%Y')
     elsif self.date_mask == 6
       return self.release_date.strftime('%b %Y')
-    elsif self.date_mask == 0
+    elsif self.date_mask == 0 || self.release_date == nil
       return 'No release date is given.'
     else
       return self.release_date.strftime('%F')
+    end
+  end
+
+  def get_date_select_opt
+    opt = { include_blank: true }
+    if self.date_mask == 4
+      date = PartialDate.new(self.release_date.year, nil, nil)
+      return opt.merge( selected: date)
+    elsif self.date_mask == 6
+      date = PartialDate.new(self.release_date.year, self.release_date.month, nil)
+      return opt.merge( selected: date)
+    elsif self.date_mask == 0 || self.release_date == nil
+      return opt.merge( selected: nil )
+    else
+      return opt
     end
   end
 end
